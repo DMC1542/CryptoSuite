33503e871b0612e9da2d02585633e1e7744af8e7
Domenic Cacace <DMC1542@rit.edu>
2022-01-23 18:31:49 -0500

    Fixed several warnings: changed switch to enhanced switch in Main.main, changed printHelp() prints to be continuous code block

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import Cipher.Affine;
-import Math.Functions;
 
 /**
  * Handles the command line arguments for the Cryptographic Services program
@@ -19,30 +18,28 @@ public class Main
      * Runs the program via the command line
      * @param args Command line arguments provided for encryption services.
      */
-    public static void main(String args[])
+    public static void main(String[] args)
     {
-        switch (args[0])
-        {
-            case "help":
-            case "-h":
-            case "-help":
-                printHelp();
-                break;
-            case "affine":
-                Affine.execute(args);
-                break;
+        switch (args[0]) {
+            case "help", "-h", "-help" -> printHelp();
+            case "affine" -> Affine.execute(args);
         }
     }
 
     public static void printHelp()
     {
         System.out.println("Usage: java -jar Cryptography.jar [cipher] [parameters]\n");
-        System.out.println("1) Affine Cipher: (affine)\n" +
-                "   -e  : Encryption\n" +
-                "   -d  : Decryption\n" +
-                "   -a [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25]\n" +     // a value used to expand keyspace
-                "   -b [value]\n" +                                         // b value used to offset shift
-                "   -i [value] (optional)\n" +                              // a's inverse, used for decryption
-                "   -m \"[message]\" : Letters only. No punctuation. Must be wrapped in quotes if there are spaces.\n");
+        // a value used to expand keyspace
+        // b value used to offset shift
+        // a's inverse, used for decryption
+        System.out.println("""
+                1) Affine Cipher: (affine)
+                   -e  : Encryption
+                   -d  : Decryption
+                   -a [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25]
+                   -b [value]
+                   -i [value] (optional)
+                   -m "[message]" : Letters only. No punctuation. Must be wrapped in quotes if there are spaces.
+                """);
     }
 }
